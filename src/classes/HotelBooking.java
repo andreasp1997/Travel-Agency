dd00230aefae884d8123a5d316bbcf9bbfc83a35
Mattias <mattejonsson@hotmail.se>
2017-04-12 10:19:07 +0200

    Added HotelBooking class

---
+++ src/classes/HotelBooking.java
@@ -0,0 +1,37 @@
+package classes;
+
+/**
+ * Created by Mattias on 2017-04-12.
+ */
+public class HotelBooking {
+    //Variables
+    private String hotelName;
+    private int roomSize;
+    private String checkinDate;
+    private String checkoutDate;
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public int getRoomSize() {
+        return roomSize;
+    }
+
+    public String getCheckinDate() {
+        return checkinDate;
+    }
+
+    public String setCheckinDate() {
+        return checkinDate;
+    }
+
+    public String getCheckoutDate() {
+        return checkoutDate;
+    }
+
+    public String setCheckoutDate() {
+        return checkoutDate;
+    }
+
+}
