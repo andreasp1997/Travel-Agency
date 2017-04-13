c4a74e587ba49c2bb501eaf4ffd1bd2f3db37820
andreasp1997 <andreas.petersson0312@stud.hkr.se>
2017-04-13 14:44:11 +0200

    added user menu controller

---
+++ src/classes/userMenuController.java
@@ -0,0 +1,29 @@
+package classes;
+
+import javafx.event.ActionEvent;
+
+/**
+ * Created by andreas on 2017-04-13.
+ */
+public class userMenuController {
+
+    public void gotoMakeBooking(ActionEvent ae){
+
+    }
+
+    public void gotoAccountSettings(ActionEvent ae){
+
+    }
+
+    public void gotoEditBookings(ActionEvent ae){
+
+    }
+
+    public void help(ActionEvent ae){
+
+    }
+
+    public void back(ActionEvent ae){
+
+    }
+}
