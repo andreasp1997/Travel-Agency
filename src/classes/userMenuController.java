6c139a9ac0dc807d4859907b99e10f470bbf7f67
andreasp1997 <andreas.petersson0312@stud.hkr.se>
2017-04-22 15:37:23 +0200

    logout function added to userMenuController

--- src/classes/userMenuController.java
+++ src/classes/userMenuController.java
@@ -1,6 +1,13 @@
 package classes;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 /**
  * Created by andreas on 2017-04-13.
@@ -23,7 +30,15 @@ public class userMenuController {
 
     }
 
-    public void back(ActionEvent ae){
+    public void logout(ActionEvent ae){
 
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("../fxmlFiles/welcomeScreen.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node) ae.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
