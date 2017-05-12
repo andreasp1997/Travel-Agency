968ebfa4916b73f085a79a74b6ed348f25e3dfc0
andreasp1997 <andreas.petersson0312@stud.hkr.se>
2017-05-12 23:58:48 +0200

    Car inserts sql file added

---
+++ resources/Car inserts.sql
@@ -0,0 +1,48 @@
+/*
+-- Query: select * from cars
+LIMIT 0, 1000
+
+-- Date: 2017-05-12 18:00
+*/
+INSERT INTO `cars` (`car_id`,`car`,`seats`,`location`,`price`,`amount`) VALUES (1,'Chevrolet Camaro',2,1,900,20);
+INSERT INTO `cars` (`car_id`,`car`,`seats`,`location`,`price`,`amount`) VALUES (2,'Dodge Charger',5,1,600,20);
+INSERT INTO `cars` (`car_id`,`car`,`seats`,`location`,`price`,`amount`) VALUES (3,'Land Rover Discovery',7,1,700,20);
+INSERT INTO `cars` (`car_id`,`car`,`seats`,`location`,`price`,`amount`) VALUES (4,'Chevrolet Camaro',2,2,900,20);
+INSERT INTO `cars` (`car_id`,`car`,`seats`,`location`,`price`,`amount`) VALUES (5,'Dodge Charger',5,2,600,20);
+INSERT INTO `cars` (`car_id`,`car`,`seats`,`location`,`price`,`amount`) VALUES (6,'Land Rover Discovery',7,2,700,20);
+INSERT INTO `cars` (`car_id`,`car`,`seats`,`location`,`price`,`amount`) VALUES (7,'Chevrolet Camaro',2,3,900,20);
+INSERT INTO `cars` (`car_id`,`car`,`seats`,`location`,`price`,`amount`) VALUES (8,'Dodge Charger',5,3,600,20);
+INSERT INTO `cars` (`car_id`,`car`,`seats`,`location`,`price`,`amount`) VALUES (9,'Land Rover Discovery',7,3,700,20);
+INSERT INTO `cars` (`car_id`,`car`,`seats`,`location`,`price`,`amount`) VALUES (10,'Chevrolet Camaro',2,4,900,20);
+INSERT INTO `cars` (`car_id`,`car`,`seats`,`location`,`price`,`amount`) VALUES (11,'Dodge Charger',5,4,600,20);
+INSERT INTO `cars` (`car_id`,`car`,`seats`,`location`,`price`,`amount`) VALUES (12,'Land Rover Discovery',7,4,700,20);
+INSERT INTO `cars` (`car_id`,`car`,`seats`,`location`,`price`,`amount`) VALUES (13,'Chevrolet Camaro',2,5,900,20);
+INSERT INTO `cars` (`car_id`,`car`,`seats`,`location`,`price`,`amount`) VALUES (14,'Dodge Charger',5,5,600,20);
+INSERT INTO `cars` (`car_id`,`car`,`seats`,`location`,`price`,`amount`) VALUES (15,'Land Rover Discovery',7,5,700,20);
+INSERT INTO `cars` (`car_id`,`car`,`seats`,`location`,`price`,`amount`) VALUES (16,'Chevrolet Camaro',2,6,900,20);
+INSERT INTO `cars` (`car_id`,`car`,`seats`,`location`,`price`,`amount`) VALUES (17,'Dodge Charger',5,6,600,20);
+INSERT INTO `cars` (`car_id`,`car`,`seats`,`location`,`price`,`amount`) VALUES (18,'Land Rover Discovery',7,6,700,20);
+INSERT INTO `cars` (`car_id`,`car`,`seats`,`location`,`price`,`amount`) VALUES (19,'Chevrolet Camaro',2,7,900,20);
+INSERT INTO `cars` (`car_id`,`car`,`seats`,`location`,`price`,`amount`) VALUES (20,'Dodge Charger',5,7,600,20);
+INSERT INTO `cars` (`car_id`,`car`,`seats`,`location`,`price`,`amount`) VALUES (21,'Land Rover Discovery',7,7,700,20);
+INSERT INTO `cars` (`car_id`,`car`,`seats`,`location`,`price`,`amount`) VALUES (22,'Chevrolet Camaro',2,8,900,20);
+INSERT INTO `cars` (`car_id`,`car`,`seats`,`location`,`price`,`amount`) VALUES (23,'Dodge Charger',5,8,600,20);
+INSERT INTO `cars` (`car_id`,`car`,`seats`,`location`,`price`,`amount`) VALUES (24,'Land Rover Discovery',7,8,700,20);
+INSERT INTO `cars` (`car_id`,`car`,`seats`,`location`,`price`,`amount`) VALUES (25,'Chevrolet Camaro',2,9,900,20);
+INSERT INTO `cars` (`car_id`,`car`,`seats`,`location`,`price`,`amount`) VALUES (26,'Dodge Charger',5,9,600,20);
+INSERT INTO `cars` (`car_id`,`car`,`seats`,`location`,`price`,`amount`) VALUES (27,'Land Rover Discovery',7,9,700,20);
+INSERT INTO `cars` (`car_id`,`car`,`seats`,`location`,`price`,`amount`) VALUES (28,'Chevrolet Camaro',2,10,900,20);
+INSERT INTO `cars` (`car_id`,`car`,`seats`,`location`,`price`,`amount`) VALUES (29,'Dodge Charger',5,10,600,20);
+INSERT INTO `cars` (`car_id`,`car`,`seats`,`location`,`price`,`amount`) VALUES (30,'Land Rover Discovery',7,10,700,20);
+INSERT INTO `cars` (`car_id`,`car`,`seats`,`location`,`price`,`amount`) VALUES (31,'Chevrolet Camaro',2,11,900,20);
+INSERT INTO `cars` (`car_id`,`car`,`seats`,`location`,`price`,`amount`) VALUES (32,'Dodge Charger',5,11,600,20);
+INSERT INTO `cars` (`car_id`,`car`,`seats`,`location`,`price`,`amount`) VALUES (33,'Land Rover Discovery',7,11,700,20);
+INSERT INTO `cars` (`car_id`,`car`,`seats`,`location`,`price`,`amount`) VALUES (34,'Chevrolet Camaro',2,12,900,20);
+INSERT INTO `cars` (`car_id`,`car`,`seats`,`location`,`price`,`amount`) VALUES (35,'Dodge Charger',5,12,600,20);
+INSERT INTO `cars` (`car_id`,`car`,`seats`,`location`,`price`,`amount`) VALUES (36,'Land Rover Discovery',7,12,700,20);
+INSERT INTO `cars` (`car_id`,`car`,`seats`,`location`,`price`,`amount`) VALUES (37,'Chevrolet Camaro',2,13,900,20);
+INSERT INTO `cars` (`car_id`,`car`,`seats`,`location`,`price`,`amount`) VALUES (38,'Dodge Charger',5,13,600,20);
+INSERT INTO `cars` (`car_id`,`car`,`seats`,`location`,`price`,`amount`) VALUES (39,'Land Rover Discovery',7,13,700,20);
+INSERT INTO `cars` (`car_id`,`car`,`seats`,`location`,`price`,`amount`) VALUES (40,'Chevrolet Camaro',2,14,900,20);
+INSERT INTO `cars` (`car_id`,`car`,`seats`,`location`,`price`,`amount`) VALUES (41,'Dodge Charger',5,14,600,20);
+INSERT INTO `cars` (`car_id`,`car`,`seats`,`location`,`price`,`amount`) VALUES (42,'Land Rover Discovery',7,14,700,20);
